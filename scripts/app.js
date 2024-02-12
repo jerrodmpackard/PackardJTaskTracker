e4d67c47e770e359dc491be5fe181c64fc015d89
jerrodmpackard <jerrodmpackard@gmail.com>
2024-02-12 15:29:42 -0800

    added local storage

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,72 @@
-// Need to create elements to add tasks to the to-do section
-// add IDs to each form input
+import { saveToLocalStorage, getLocalStorage } from "./localstorage.js";
 
 let taskName = document.getElementById('taskName');
 let date = document.getElementById('date');
 let description = document.getElementById('description');
 let priority = document.getElementById('priority');
 let status = document.getElementById('status');
-let addTask = document.getElementById('addTask');
+let addTaskBtn = document.getElementById('addTaskBtn');
+let toDoColumn = document.getElementById('toDoColumn');
+let inProgressColumn = document.getElementById('inProgressColumn');
+let completedColumn = document.getElementById('completedColumn');
 
 
-addTask.addEventListener("click", () => {
-    let p = document.createElement("p");
-    p.textContent = taskName.value;
-})
+addTaskBtn.addEventListener("click", () => {
+
+    const newObject = {
+        taskName: taskName.value,
+        date: date.value,
+        description: description.value,
+        priority: priority.value,
+        status: status.value
+    }
+
+    saveToLocalStorage(newObject);
+
+    displayTasks();
+
+});
+
+const displayTasks = () => {
+    let tasks = getLocalStorage();
+
+    let pName = document.createElement("p");
+    let pDate = document.createElement("p");
+    let pDescription = document.createElement("p");
+    let pPriority = document.createElement("p");
+    let pStatus = document.createElement("p");
+
+    pName.textContent = newObject.taskName;
+    pDate.textContent = newObject.date;
+    pDescription.textContent = newObject.description;
+    pPriority.textContent = newObject.priority;
+    pStatus.textContent = newObject.status;
+
+    tasks.map(task => {
+        if(task.priority == "To-Do"){
+            toDoColumn.append(pName);
+            toDoColumn.append(pDate);
+            toDoColumn.append(pDescription);
+            toDoColumn.append(pPriority);
+            toDoColumn.append(pStatus);
+        } else if(task.priority == "In Progress"){
+            inProgressColumn.append(pName);
+            inProgressColumn.append(pDate);
+            inProgressColumn.append(pDescription);
+            inProgressColumn.append(pPriority);
+            inProgressColumn.append(pStatus);
+        } else {
+            completedColumn.append(pName);
+            completedColumn.append(pDate);
+            completedColumn.append(pDescription);
+            completedColumn.append(pPriority);
+            completedColumn.append(pStatus);
+        }
+    });
+
+}
+
+// displayTasks();
+
+// need to create 5 elements for each of the 5 info points and also 1 button
+// need to map through newObject.priority. need if statement to see where to create the element
--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,1 +1,23 @@
-// Need a saveToLocalStorage function and getLocalStorage function
+const saveToLocalStorage = (task) => {
+    let tasks = getLocalStorage();
+
+    if (!tasks.includes(task)) {
+        tasks.push(task);
+    }
+
+    localStorage.setItem("Tasks", JSON.stringify(tasks));
+
+}
+
+const getLocalStorage = () => {
+    let localStorageData = localStorage.getItem("Tasks");
+
+    if(localStorageData == null){
+        return [];
+    }
+
+    return JSON.parse(localStorageData);
+
+}
+
+export {saveToLocalStorage, getLocalStorage};
